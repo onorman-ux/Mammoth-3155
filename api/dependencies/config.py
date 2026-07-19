577305f63ebfcfe8053d5797afd7a6707e32cb57
owen <cantreadmyname11@gmail.com>
2026-07-19 19:09:48 -0400

    chore: configure project database

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,11 @@
+import os
+
+
 class conf:
     db_host = "localhost"
     db_name = "mammoth_3155"
     db_port = 3306
     db_user = "mammoth_app"
-    db_password = "mammoth3155local"
+    db_password = os.getenv("MAMMOTH_DB_PASSWORD", "")
     app_host = "localhost"
     app_port = 8000
