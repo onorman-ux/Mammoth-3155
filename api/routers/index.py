80fd960b1032330a0992137ae06f9f37fc1b4fba
Mika <2fordiscord@gmail.com>
2026-08-02 19:15:06 -0400

    added  menu item CRUD endpoints

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,7 +1,8 @@
-from . import orders, order_details, resources
+from . import orders, order_details, resources, menu_items
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
     app.include_router(resources.router)
+    app.include_router(menu_items.router)
