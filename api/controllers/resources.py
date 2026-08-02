dde32b337a2b277252e962a9ea40dcc5741ddd1b
Mika <2fordiscord@gmail.com>
2026-08-02 18:54:02 -0400

    fix: resolve resource CRUD merge conflicts

--- api/controllers/resources.py
+++ api/controllers/resources.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import HTTPException, Response, status
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
@@ -56,38 +55,3 @@ def delete(db: Session, item_id: int):
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     except SQLAlchemyError as exc:
         _db_error(db, exc)
-=======
-from sqlalchemy.orm import Session
-from fastapi import HTTPException, status, Response, Depends
-from ..models import models, schemas
-
-
-def create(db: Session, resource):
-    db_resource = models.Resource(
-        resource_item=resource.item,
-        resource_amount=resource.amount
-    )
-    db.add(db_resource)
-    db.commit()
-    db.refresh(db_resource)
-    return db_resource
-
-def read_all(db: Session):
-    return db.query(models.Resource).all()
-
-def read_one(db: Session, resource_id):
-    return db.query(models.Resource).filter(models.Resource.id == resource_id).first()
-
-def update(db: Session, resource_id, resource):
-    db_resource = db.query(models.Resource).filter(models.Resource.id == resource_id)
-    update_data = resource.model_dump(exclude_unset=True)
-    db_resource.update(update_data, synchronize_session=False)
-    db.commit()
-    return db_resource.first()
-
-def delete(db: Session, resource_id):
-    db_resource = db.query(models.Resource).filter(models.Resource.id == resource_id)
-    db_resource.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
->>>>>>> origin/main
